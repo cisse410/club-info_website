098b36a8a2efb760146206292aa677d8bdada8bc
Mouhamedoune  Fall <MEDOUNEFALL126@GMAIL.COM>
2023-11-02 21:22:44 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Footer from './components/footer';
+import Header from './components/header.jsx'
+import HeroSection from './components/heroSection.jsx' 
+// import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="overflow-x-hidden h-screen w-full all">
+      {/* <Router>
+        <Routes>
+          <Route path='\' element={}/>
+        </Routes>
+      </Router> */}
+      <Header/>
+      <HeroSection/> 
+      <Footer/>
+
     </div>
   );
 }
